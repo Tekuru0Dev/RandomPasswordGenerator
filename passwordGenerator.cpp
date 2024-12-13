d77ceef7fea09114ecc2240acda2d70c198c484f
Dev <dev.devcool55@gmail.com>
2024-12-13 20:14:54 +0530

    Update passwordGenerator.cpp
    Added comments

--- passwordGenerator.cpp
+++ passwordGenerator.cpp
@@ -8,19 +8,23 @@ using namespace std;
 
 string randomPasswrodGenerator(int length)
 {
+    //creating all possible values
     const string upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
     const string lowerCase = "abcdefghijklmnopqrstuvwxyz";
     const string digits = "0123456789";
     const string specialLetters = "!@#$%^&*()-=_+";
     const string allChar = upperCase + lowerCase + digits + specialLetters;
 
+    //defining out variable/password
     string password;
 
+    //ensuring atleast 1 of each character type is used
     password += upperCase[rand() % upperCase.length()];
     password += lowerCase[rand() % lowerCase.length()];
     password += digits[rand() % digits.length()];
     password += specialLetters[rand() % specialLetters.length()];
 
+    //randomly select from all characters the rest of the password
     for(int i = 4; i < length; i++)
     {
         password += allChar[rand() % allChar.length()];
@@ -29,7 +33,7 @@ string randomPasswrodGenerator(int length)
     // Create a random number generator
     random_device rd; // Random device for seed
     mt19937 rng(rd()); // Mersenne Twister generator
-    shuffle(password.begin(),password.end(),rng);
+    shuffle(password.begin(),password.end(),rng); //shuffle the password to create properly random password
 
     return password;
 }
